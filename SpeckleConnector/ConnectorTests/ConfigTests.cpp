882e12d24d7603d4bb3097956701d6c0180f6aa8
Ralph Wessel <ralph@speckle.systems>
2024-08-22 09:16:39 +0100

    Added skeleton for unit tests - to be fleshed out as time allows

---
+++ SpeckleConnector/ConnectorTests/ConfigTests.cpp
@@ -0,0 +1,15 @@
+#include "ActiveLibDoctest/TestingPlatforms.h"
+
+//Placeholder for JS configuration binding tests - implement as time allows
+
+TEST_SUITE(TESTQ(ConfigTests)) TEST_SUITE_OPEN
+	
+		//Test getting the current configuration
+	TEST_CASE(TESTQ(testConfigGet)) {
+	}
+
+		//Test updating the current configuration
+	TEST_CASE(TESTQ(testConfigUpdate)) {
+	}
+
+TEST_SUITE_CLOSE
