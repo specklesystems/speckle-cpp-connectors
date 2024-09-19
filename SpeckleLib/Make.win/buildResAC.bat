6e0cc4d5f38155a46d19c89e3172e1fda1b539c1
Ralph Wessel <ralph@speckle.systems>
2024-09-19 11:23:19 +0100

    Updated VS projects and resources More document info retrieved from open project

--- SpeckleLib/Make.win/buildResAC.bat
+++ SpeckleLib/Make.win/buildResAC.bat
@@ -3,6 +3,7 @@ REM AC Resource build script
 ECHO "Building AC Resources"
 
 if not exist "%RES_OUTPUT%" MD "%RES_OUTPUT%"
-python "%HEADER_PATH_5%\Tools\CompileResources.py" "INT" "%HEADER_PATH_5%\.." "%HEADER_PATH_6%Source" "%SYMROOT%\" "%RES_OUTPUT%" "%RES_SOURCE%\CodesignImport.apx.mui"
+python "%HEADER_PATH_5%\Tools\CompileResources.py" "INT" "%HEADER_PATH_5%\.." "%HEADER_PATH_6%\Connector" "%SYMROOT%\" "%RES_OUTPUT%" "%RES_SOURCE%\Speckle Connector.apx.mui"
+python "%HEADER_PATH_5%\Tools\CompileResources.py" "INT" "%HEADER_PATH_5%\.." "%HEADER_PATH_4%\Speckle" "%HEADER_PATH_4%" "%RES_OUTPUT%" "%RES_SOURCE%\Speckle Connector.apx.mui"
 
 ECHO "Finished AC Resource"
