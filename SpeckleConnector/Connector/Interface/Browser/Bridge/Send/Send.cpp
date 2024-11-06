0e0dc7da2dda339b37ed803bd506b7275ac3ae01
David Kekesi <david@speckle.systems>
2024-11-06 20:24:03 +0100

    switching to 3D window before send operation implemented

--- SpeckleConnector/Connector/Interface/Browser/Bridge/Send/Send.cpp
+++ SpeckleConnector/Connector/Interface/Browser/Bridge/Send/Send.cpp
@@ -44,6 +44,11 @@ Send::Send() : BridgeMethod{"Send", [&](const SendArgs& args) {
 	modelCardID: The ID of the model card identifying the objects to send
   --------------------------------------------------------------------*/
 void Send::run(const String& modelCardID) const {
+
+	// we currently rely on the ModelerAPI instead of the ModelAccessAPI
+	// we have to open a 3D window to get the Mesh data of the model elements
+	ACAPI_View_ShowAllIn3D();
+
 		//Get the active project
 	auto project = connector()->getActiveProject().lock();
 	if (!project) {
