e9dff1a12d6f0e5f042aa8780b507fee29fe53a2
Ralph Wessel <ralph@speckle.systems>
2024-11-14 16:20:24 +0000

    Zoom and zoom-to-selection reversed

--- SpeckleLib/Speckle/Environment/Host.cpp
+++ SpeckleLib/Speckle/Environment/Host.cpp
@@ -63,13 +63,13 @@ void Host::zoomToFit(bool isSelectionOnly) const {
 #ifdef ARCHICAD
 	if (isSelectionOnly)
 #ifdef ServerMainVers_2700
-		ACAPI_View_Zoom();
+		ACAPI_View_ZoomToSelected();
 #else
 		ACAPI_Automate(APIDo_ZoomToSelectedID);
 #endif
 	else
 #ifdef ServerMainVers_2700
-		ACAPI_View_ZoomToSelected();
+		ACAPI_View_Zoom();
 #else
 		ACAPI_Automate(APIDo_ZoomID);
 #endif
